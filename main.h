4054feeb9f220f41a9fd3c1769aae2b9ff2cfea9
adil mae <itsadilhadid@gmail.com>
2023-04-22 20:05:02 +0000

    alx

--- main.h
+++ main.h
@@ -11,7 +11,7 @@
 
 /* 0_task prototypes */
 int my_strlen(const char *str);
-void my_putchar(const char c);
+int my_putchar(const char c);
 void print_string(const char *string);
 void check_char(va_list myarg, int *count_char);
 void check_string(va_list myarg, int *count_char);
