a33d128ad8d232f5796a1ba24103ef6bcfb3dd3c
adil mae <itsadilhadid@gmail.com>
2023-04-23 23:44:14 +0100

    alx

--- _printf.c
+++ _printf.c
@@ -65,6 +65,11 @@ int _printf(const char *format, ...)
 	va_list args;
 
 	va_start(args, format);
+
+	if (!format || (format[0] == '%' && !format[1]))
+		return (-1);
+	if (format[0] == '%' && format[1] == ' ' && !format[2])
+		return (-1);
 	for (i = 0; format[i] != '\0'; i++)
 	{
 		if (format[i] == '%')
