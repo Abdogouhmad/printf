09ec4657830d88acbc9d2a34cfe2609ba22b7c2e
musashi <gouhmada@gmail.com>
2023-04-24 01:06:38 +0100

    updated

--- _printf.c
+++ _printf.c
@@ -1,18 +1,89 @@
-#include  "main.h"
+#include <unistd.h>
+#include <stdarg.h>
+#include <stdio.h>
+#include <string.h>
 
 
 /**
- * _printf - prints formatted output to standard output.
+ * handle_char - handles a single character conversion specifier
  *
+ * @specifier: the conversion specifier character
+ * @args: the va_list of arguments
+ * @count: a pointer to the running count of printed characters
+ * @width: the width specifier (optional)
+ * @precision: the precision specifier (optional)
+ */
+void handle_char(char specifier, va_list args, int *count, int width, int precision)
+{
+	char *str;
+	int len, i = 0;
+
+	switch (specifier)
+	{
+		case 'c':
+		{
+			int c = va_arg(args, int);
+
+			write(1, &c, 1);
+			(*count)++;
+			break;
+		}
+		case 's':
+		{
+			str = va_arg(args, char *);
+
+			if (str == NULL)
+				str = "(null)";
+
+			len = strlen(str);
+
+			if (precision >= 0 && precision < len)
+				len = precision;
+
+			if (width > len)
+			{
+				if (precision < 0)
+					precision = len;
+
+				if (width > precision)
+				{
+					for (i = 0; i < width - len; i++)
+					{
+						write(1, " ", 1);
+						(*count)++;
+					}
+				}
+			}
+
+			write(1, str, len);
+			(*count) += len;
+			break;
+		}
+		case '%':
+		{
+			write(1, "%", 1);
+			(*count)++;
+			break;
+		}
+		default:
+		{
+			write(1, &specifier, 1);
+			(*count)++;
+			break;
+		}
+	}
+}
+
+/**
+ * _printf - prints formatted output to a stream
+ *
+ * @stream: the output stream
  * @format: a string containing format specifiers that control the output.
  *          Supported format specifiers are %c, %s, and %%.
- *          Any other characters are printed as-is.
  * @...: optional arguments to be printed according to the format specifiers.
- *@stream:  the stream to print to.
- * Return: .....
- * or -1 if an error occurs.
+ *
+ * Return: the number of characters printed, or -1 on error.
  */
-
 int _printf(FILE *stream, const char *format, ...)
 {
 	int count = 0, i, width = 0, precision = -1;
